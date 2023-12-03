b1438043dde2b498bd54b7e74d91be1da421ee28
lenayoo <lenayoo17@gmail.com>
2023-12-03 18:35:41 +0900

    making card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,9 +9,11 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ imgSrc, title, content }) => {
   return (
     <div className="card">
-      <img src={imgSrc} className="card-img" />
-      <div>{title}</div>
-      <div>{content}</div>
+      <div className="card-wrapper">
+        <img src={imgSrc} className="card-img" />
+        <div className="card-title">{title}</div>
+        <div className="card-content">{content}</div>
+      </div>
     </div>
   );
 };
