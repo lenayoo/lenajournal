1ecf30eb66e39b93d65ac67fc0522843b6aa8ce3
lenayoo <lenayoo17@gmail.com>
2023-12-03 16:58:20 +0900

    add flowbite

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Nav from "./Nav";
+import { Button } from "flowbite-react";
 
 function Home() {
   return (
     <>
       <Nav />
       <div>Home</div>
+      <Button> Click me</Button>
     </>
   );
 }
